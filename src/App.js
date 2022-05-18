80ca5af039f9d0345873dc800691f3b6d68f52e0
luanaschuler <66922010+luanashuler@users.noreply.github.com>
2022-05-18 09:51:51 -0300

    filtered places by type and rating

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import Map from './components/Map/Map';
 const App = () => {
 
   const [places, setPlaces] = useState([]);
+  const [ filteredPlaces, setFilteredPlaces ] = useState([]);
   const [childClicked, setChildClicked] = useState(null);
 
   const [ coordinates, setCoordinates ] = useState({});
   const [ bounds, setBounds ] = useState({});
-  const [ isLoading, setIsLoading] = useState(false)
+  const [ isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
 
 
   useEffect(() => {
@@ -23,15 +26,22 @@ const App = () => {
   }, []);
 
   useEffect(() => {
+    const filteredPlaces = places.filter((place) => place.rating > rating);
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
 
     setIsLoading(true)
 
-    getPlacesData(bounds.sw, bounds.ne)
+    getPlacesData(type, bounds.sw, bounds.ne)
       .then((data) => {
         setPlaces(data);
+        setFilteredPlaces([]);
         setIsLoading(false);
       })
-  }, [bounds]);
+  }, [type, bounds]);
+
 
   return(
     <>
@@ -40,7 +50,15 @@ const App = () => {
       <Grid container spacing={3} style={{ width: '100%' }}>
         
         <Grid item xs={12} md={4}>
-          <List places={places} childClicked={childClicked} isLoading={isLoading} />
+          <List 
+            places={ filteredPlaces.length ? filteredPlaces : places} 
+            childClicked={childClicked} 
+            isLoading={isLoading} 
+            type={type} 
+            setType={setType} 
+            rating={rating} 
+            setRating={setRating} 
+          />
         </Grid>
 
         <Grid item xs={12} md={8}>
@@ -48,7 +66,7 @@ const App = () => {
             setCoordinates={setCoordinates} 
             setBounds={setBounds} 
             coordinates={coordinates}
-            places={places}
+            places={filteredPlaces.length ? filteredPlaces : places} 
             setChildClicked={setChildClicked}
             
           />
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,12 +3,10 @@ import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, 
 import PlaceDetails from '../PlaceDetails/PlaceDetails'
 import useStyles from './styles';
 
-const List = ({ places, childClicked, isLoading }) => {
+const List = ({ places, childClicked, isLoading, type, setType, rating, setRating }) => {
 
   const classes = useStyles();
 
-  const [type, setType] = useState('restaurants');
-  const [rating, setRating] = useState('');
   const [elRefs, setElRefs] = useState([]);
 
   useEffect(() => {
